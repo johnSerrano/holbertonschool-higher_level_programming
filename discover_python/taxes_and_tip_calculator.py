9d023e522e51836663fd4d9145599d9f799fd004
John Serrano <johnserrano404@gmail.com>
2016-02-06 12:38:16 -0800

    comments

--- discover_python/taxes_and_tip_calculator.py
+++ discover_python/taxes_and_tip_calculator.py
@@ -1,5 +1,14 @@
+"""This program calculates
+the amount you should pay
+including tip and tax"""
+
+#welcome line
 print("Welcome to the taxes and tip calculator!")
+#price input and prompt
 price = float(input("What is the price before tax? "))
+#tax input and prompt
 tax = float(input("What are the taxes? (in %) "))
+#tip input and prompt
 tip = float(input("What do you want to tip? (in %) "))
+#calculate price and print
 print("the price you need to pay is " + str(((price*tax/100) + ((price*tax/100 + price)*tip/100) + price)) + ".")
