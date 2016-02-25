c667419b8add0275414225e71fb4695a974023c7
John Serrano <johnserrano404@gmail.com>
2016-02-24 16:37:20 -0800

    fixed api key issue

--- discover_javascript/github_most_popular_1.js
+++ discover_javascript/github_most_popular_1.js
@@ -5,13 +5,13 @@ var options = {
 	path: '/search/repositories?q=language:javascript&sort=stars&order=desc',
 	headers: {
 		'User-Agent': 'Holberton_School',
-		'Authorization': 'token d2066dda3c6bf142b5b0ac9f789de960d35e4e05',
+		'Authorization': 'token f4f6caa6a3e5f5da491e42d6ee14708f325ad655',
 		'Accept': '*/*'
 	}
 }
 
+
 var req = https.request(options, function(res) {
-	console.log(res.statusCode);
 	res.on('data', function(d) {
 		process.stdout.write(d);
 	});
