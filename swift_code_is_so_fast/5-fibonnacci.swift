e319c6fe460139bc90511551e3182694767ff015
john serrano <johnserrano404@gmail.com>
2016-05-24 05:26:43 +0000

    dd 6 reverse array

--- swift_code_is_so_fast/5-fibonnacci.swift
+++ swift_code_is_so_fast/5-fibonnacci.swift
@@ -7,6 +7,6 @@ func fibonacci(number: Int) -> (Int) {
 	return fast
 }
 
-print("Fibonnacci of 3 is \(fibonacci(3))")
-print("Fibonnacci of 7 is \(fibonacci(7))")
-print("Fibonnacci of 18 is \(fibonacci(18))")
+//print("Fibonnacci of 3 is \(fibonacci(3))")
+//print("Fibonnacci of 7 is \(fibonacci(7))")
+//print("Fibonnacci of 18 is \(fibonacci(18))")
