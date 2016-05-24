1fbe7dd0a6927393d0ace2c7a0036819f3b56a89
John Serrano <johnserrano404@gmail.com>
2016-05-24 09:32:30 -0700

    comment out test lines

--- swift_code_is_so_fast/3-longest_word.swift
+++ swift_code_is_so_fast/3-longest_word.swift
@@ -10,5 +10,5 @@ func longest_word(s: String) -> String {
   return words.reduce(string, combine:{longer_word($0, $1)})
 }
 
-var my_text = "I don't know which word will be the longest of this sentence, so I will try to find it!"
-print("\"\(longest_word(my_text))\" is the longest word in \"\(my_text)\"")
+//var my_text = "I don't know which word will be the longest of this sentence, so I will try to find it!"
+//print("\"\(longest_word(my_text))\" is the longest word in \"\(my_text)\"")
