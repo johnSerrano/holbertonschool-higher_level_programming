4c5f15308030a98f204f92d3d4bfffd888b5a2c1
John Serrano <24@p4-1.hbtn.io>
2016-05-24 13:21:24 -0700

    swift object project task 1

--- manage_the_school_with_swift/person.swift
+++ manage_the_school_with_swift/person.swift
@@ -14,7 +14,34 @@ class Person {
   }
 }
 
+protocol Classify {
+  func isStudent() -> Bool
+}
+
 // var p = Person(first_name: "John", last_name: "Fring", age: 30)
 //
 // print("name: \(p.first_name)")
 // print("fullName: \(p.fullName())")
+class Mentor: Person, Classify {
+  func isStudent() -> Bool {
+    return false
+  }
+}
+
+class Student: Person, Classify {
+  func isStudent() -> Bool {
+    return true
+  }
+}
+
+var s = Student(first_name: "Sam", last_name: "Scoth", age: 20)
+var t = Mentor(first_name: "Alex", last_name: "Rap", age: 34)
+
+if s.isStudent()
+{
+    print("\(s.fullName()) is student")
+}
+if t.isStudent()
+{
+    print("\(t.fullName()) is student")
+}
